8e5dae3ca265d3879cde666632ca9b19ed773620
ramu bandela <ramu.548949@gmail.com>
2025-11-13 13:09:05 +0530

    new interview questions

---
+++ src/main/java/com/tesco/integration/hcm_jda/interview/questions/groupby/FirstNonRepeatingCharacter.java
@@ -0,0 +1,19 @@
+package com.tesco.integration.hcm_jda.interview.questions.groupby;
+
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+
+        /*String s="abcbcdbcd";
+        LinkedHashMap<Character, Character> map = s.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(c -> c, LinkedHashMap::new, Collectors.collectingAndThen(
+                Collectors.toList(),
+                tolIst -> tolIst.stream().filter(x -> x == 1).findFirst().orElse('$')
+        )));
+
+        System.out.println(map);*/
+
+        List.
+    }
+}
